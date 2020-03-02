8b72599e0ef4a2528918e5d2be564371df212f4f
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-03-02 20:32:49 +0200

    finsh lab 12

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 // array of the name of images
+console.log(document.URL);
 var images = [
   'bag.jpg',
   'banana.jpg',
@@ -24,6 +25,7 @@ var images = [
 ];
 
 var imagesObj = [];
+var imgsIndex = [];
 var rounds = 0;
 var left;
 var ceneter;
@@ -33,6 +35,7 @@ var leftImage = document.querySelector('#img-left');
 var rightImage = document.querySelector('#img-right');
 var ceneterImage = document.querySelector('#img-ceneter');
 var resulte = document.querySelector('#resulte');
+var names;
 // Object Constructors for the images
 function Image(img) {
   this.name = img.split('.')[0];
@@ -52,22 +55,35 @@ function getRndInteger(min, max) {
 for (let index = 0; index < images.length; index++) {
   new Image(images[index]);
 }
+names = imagesObj.map(a => a.name);
 
 function randomImages() {
   // assaien the images object in varibels
-  left = imagesObj[getRndInteger(0, images.length - 1)];
-  ceneter = imagesObj[getRndInteger(0, images.length - 1)];
-  right = imagesObj[getRndInteger(0, images.length - 1)];
-  // check if there images looks same if yes will reassaien the images
-  while (
-    left.name === ceneter.name ||
-    left.name === right.name ||
-    ceneter.name === right.name
-  ) {
-    left = imagesObj[getRndInteger(0, images.length - 1)];
-    ceneter = imagesObj[getRndInteger(0, images.length - 1)];
-    right = imagesObj[getRndInteger(0, images.length - 1)];
+  /////////////////////////////
+  for (let index = 0; index < 3; index++) {
+    var i = getRndInteger(0, images.length - 1);
+    while (imgsIndex.includes(i)) {
+      i = getRndInteger(0, images.length - 1);
+    }
+    imgsIndex.push(i);
   }
+  if (imgsIndex.length > 3) {
+    imgsIndex.splice(0, 3);
+  } // alert('jlkj')
+  //////////////////////////////
+  left = imagesObj[imgsIndex[0]];
+  ceneter = imagesObj[imgsIndex[1]];
+  right = imagesObj[imgsIndex[2]];
+  // check if there images looks same if yes will reassaien the images
+  // while (
+  //   left.name === ceneter.name ||
+  //   left.name === right.name ||
+  //   ceneter.name === right.name
+  // ) {
+  //   left = imagesObj[getRndInteger(0, images.length - 1)];
+  //   ceneter = imagesObj[getRndInteger(0, images.length - 1)];
+  //   right = imagesObj[getRndInteger(0, images.length - 1)];
+  // }
   // assaien the img elements in html page with the values its need and plus that its used
   left.viwe++;
   right.viwe++;
@@ -85,6 +101,57 @@ function userClick(e) {
   // if statment to check if the user had 25 rounds if yes will delete the event and display the
   // reuslte as unoreder list
   if (rounds === 25) {
+    var main = document.getElementById('main');
+    main.style.display = 'none';
+    ///////////////
+    var votes = [];
+    var viwes = [];
+    imagesObj.forEach(a => {
+      votes.push(a.clicks);
+      viwes.push(a.viwe);
+    });
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: names,
+        datasets: [
+          {
+            label: ' Votes',
+            data: votes,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+
+            borderWidth: 1
+          },
+          {
+            label: 'Viawes',
+            data: viwes,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
+
+    ctx.style.height = '480px';
+    ctx.style.width = '90%';
+    ctx.style.display = 'block';
+    ctx.style.margin = '5px 5%';
+
+    //////////////
+
     imgs.removeEventListener('click', userClick);
     console.log(resulte);
     for (let index = 0; index < imagesObj.length; index++) {
@@ -107,5 +174,9 @@ function userClick(e) {
   randomImages();
 }
 // event listener to the images will call userClick function
+
 var imgs = document.querySelector('#all-imgs');
 imgs.addEventListener('click', userClick);
+// window.location.href = "resulte.html";
+
+// console.log(myChart);
